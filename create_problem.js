ee0846a89d431f5a1913b5ee0de3cd9a325abdd8
Bubbler <bubbler9903@gmail.com>
2025-10-04 09:28:57 +0000

    Rewrite in js and open created file in vscode window

---
+++ create_problem.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const { execFile } = require('child_process');
+
+const probNum = process.argv[2];
+const template = `---
+params:
+  boj: ${probNum}
+  bojTitle: 문제 제목
+---
+
+## 문제 내용
+
+여기에 문제 내용 입력
+
+### 입력
+
+### 출력
+
+## 문제 풀이
+
+{{% details title="스포일러" closed="true" %}}
+
+여기에 문제 풀이 입력
+
+{{% /details %}}
+`;
+
+const probNumSplit = probNum.split('');
+const fileName = probNumSplit.pop() + '.md';
+const filePath = path.join('content', 'problems', ...probNumSplit, fileName);
+
+if (!fs.existsSync(filePath)) {
+    const dirName = path.dirname(filePath);
+    if (!fs.existsSync(dirName)) {
+        fs.mkdirSync(dirName, { recursive: true });
+    }
+    fs.writeFileSync(filePath, template);
+}
+
+execFile('code', ['-r', filePath]);
